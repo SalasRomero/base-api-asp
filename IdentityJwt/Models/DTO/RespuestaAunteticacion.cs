80df7eb1d247c7e8849af90e9a617acc0c711d8c
alejandro salas <da_sa1998@hotmail.com>
2024-11-18 10:43:53 -0500

    Tipo:Feat, Mensaje:Funcionalidad de JWT con Identity

---
+++ IdentityJwt/Models/DTO/RespuestaAunteticacion.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityJwt.Models.DTO
+{
+    public class RespuestaAunteticacion
+    {
+        public string Token { get; set; }
+
+        public DateTime Expiracion { get; set; }
+    }
+}
